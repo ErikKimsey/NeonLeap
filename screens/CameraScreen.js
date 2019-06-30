1e329c48036e3fa385c562f67e4c863db408a9b2
Erik Kimsey <kimseyerik@gmail.com>
2019-06-30 16:30:27 -0400

    Building out camerascreen screen

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -7,7 +7,24 @@ export default class CameraScreen extends Component {
 	state = {
 		hasCameraPermission: null
 	};
+
+	async componentDidMount() {
+		const camera = await Permissions.askAsync(Permissions.CAMERA);
+		const hasCameraPermission = camera.status === 'granted';
+		this.setState({ hasCameraPermission });
+	}
+
 	render() {
-		return <View />;
+		const { hasCameraPermission } = this.state;
+		if (hasCameraPermission === null) {
+			<View />;
+		} else if (hasCameraPermission === false) {
+			return <Text>Denied camera access</Text>;
+		}
+		return (
+			<View>
+				<Camera style={styles.preview} ref={(camera) => (this.camera = camera)} />
+			</View>
+		);
 	}
 }
